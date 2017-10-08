d45d835061cff5165e100ec8ef5d91e800c802c0
Kenneth Blomqvist <kekeblom@gmail.com>
2017-10-08 15:38:29 +0300

    data conversion script

---
+++ convert.py
@@ -0,0 +1,32 @@
+import tqdm
+import os
+import numpy as np
+from PIL import Image
+colors = [(0,0,0)
+                # 0=background
+                ,(128,0,0),(0,128,0),(128,128,0),(0,0,128),(128,0,128)
+                # 1=aeroplane, 2=bicycle, 3=bird, 4=boat, 5=bottle
+                ,(0,128,128),(128,128,128),(64,0,0),(192,0,0),(64,128,0)
+                # 6=bus, 7=car, 8=cat, 9=chair, 10=cow
+                ,(192,128,0),(64,0,128),(192,0,128),(64,128,128),(192,128,128)
+                # 11=diningtable, 12=dog, 13=horse, 14=motorbike, 15=person
+                ,(0,64,0),(128,64,0),(0,192,0),(128,192,0),(0,64,128)]
+                # 16=potted plant, 17=sheep, 18=sofa, 19=train, 20=tv/monitor
+
+palette = {}
+for i, color in enumerate(colors):
+    palette[color] = i
+
+def convert_from_color_segmentation(arr_3d):
+    arr_2d = np.zeros((arr_3d.shape[0], arr_3d.shape[1]), dtype=np.uint8)
+
+    for c, i in palette.items():
+        m = np.all(arr_3d == np.array(c).reshape(1, 1, 3), axis=2)
+        arr_2d[m] = i
+
+    return arr_2d
+
+
+
+
+
